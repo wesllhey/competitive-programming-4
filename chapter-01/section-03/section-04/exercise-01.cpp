f9e1bdacc6484553c8dfb4c6453955c8c6c3dbeb
Wesllhey Holanda <wesllhey.holanda@gmail.com>
2021-05-14 21:25:27 -0300

    chapter -01 section 03

---
+++ chapter-01/section-03/section-04/exercise-01.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie();
+  
+  double n;
+
+  scanf("%lf", &n);
+
+  printf("%7.3lf\n", n);
+}
+
---
+++ chapter-01/section-03/section-04/exercise-02.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie();
+  
+  int n;
+
+  scanf("%d", &n);
+
+  printf("%.*lf\n", n, M_PI);
+}
+
